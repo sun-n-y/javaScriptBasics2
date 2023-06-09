e3b93439d90b5dc560e14f8e6051fef134c60294
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-09 16:19:08 -0400

    add numbers lesson and challenge

---
+++ 03 numbers/challenge/challenge.js
@@ -0,0 +1,20 @@
+//
+const score1 = 98;
+const score2 = 75;
+const score3 = 45;
+
+const totalScore = score1 + score2 + score3;
+const avgScore = totalScore / 3;
+
+console.log(totalScore, avgScore);
+
+let plates = 20;
+let people = 7;
+
+let remainingPlates = plates % people;
+
+remainingPlates++;
+
+const msg = 'There are ' + remainingPlates + ' plates available';
+
+console.log(msg);
