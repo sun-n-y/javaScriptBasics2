a4fb981d363883d2008d0005bcfacc97f1ac64a1
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-10 22:17:24 -0400

    add objects lesson

---
+++ 06 objects/lesson/lesson.js
@@ -0,0 +1,13 @@
+//
+const person = {
+  name: 'joe',
+  sport: 'volleyball',
+  favoritNumber: 11,
+  edu: false,
+  array: ['a', 's', 'r', 's', 'd'],
+  greet() {
+    return console.log('hi');
+  },
+};
+
+person.greet();
