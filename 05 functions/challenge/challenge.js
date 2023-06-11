1aaa9fbcdb2bba77505561053e85d193bef544e2
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-10 22:00:50 -0400

    add function challenge

--- 05 functions/challenge/challenge.js
+++ 05 functions/challenge/challenge.js
@@ -1,1 +1,14 @@
 //
+function calculateTotal(subTotal, tax) {
+  return subTotal + tax;
+}
+
+const sum = function (subTotal, tax) {
+  return subTotal + tax;
+};
+
+var oder1 = calculateTotal(10, 5);
+var oder2 = calculateTotal(20, 5);
+var oder3 = sum(30, 5);
+
+console.log([oder1, oder2, oder3]);
--- 05 functions/lesson/lesson.js
+++ 05 functions/lesson/lesson.js
@@ -11,13 +11,13 @@
 // hello(4, 'p');
 // hello(bob, 'bob');
 
-const w = calculate('2');
-const h = calculate(0);
+// const w = calculate('2');
+// const h = calculate(0);
 
-function calculate(value) {
-  console.log('the value in cm is : ' + value * 2.54 + ' cm');
-  return value;
-}
+// function calculate(value) {
+//   console.log('the value in cm is : ' + value * 2.54 + ' cm');
+//   return value;
+// }
 
-const dim = [w, h];
-console.log(dim);
+// const dim = [w, h];
+// console.log(dim);
