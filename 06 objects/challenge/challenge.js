73f76faf5daee7bca8eca083e606efe97fe5f318
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-10 22:28:45 -0400

    add object challenge

--- 06 objects/challenge/challenge.js
+++ 06 objects/challenge/challenge.js
@@ -1,1 +1,23 @@
 //
+const car = {
+  make: 'toyota',
+  model: 'tacoma',
+  year: '2006',
+  colors: ['silver', 'black', 'gray'],
+  hybrid: false,
+  drive(num) {
+    console.log('driving ' + num);
+  },
+  stop: function () {
+    console.log('stop');
+  },
+};
+
+console.log(car.make);
+console.log(car.colors[0]);
+
+car.drive(8);
+
+car.stop();
+
+console.log(car);
