8367d5712bf90a7a03398a3d662315f2a1cbeebf
sunny patel <sunnykpatel.skp@gmail.com>
2023-06-11 23:44:18 -0400

    add conditionals challenge

--- 07 conditionals/challenge/challenge.js
+++ 07 conditionals/challenge/challenge.js
@@ -1,1 +1,17 @@
 //
+const person1 = {
+  name: 'polo',
+  age: 18,
+  status: 'resident',
+};
+const person2 = {
+  name: 'yolo',
+  age: 17,
+  status: 'tourist',
+};
+
+if (person2.age >= 18 && person2.status === 'resident') {
+  console.log('true');
+} else {
+  console.log('false');
+}
